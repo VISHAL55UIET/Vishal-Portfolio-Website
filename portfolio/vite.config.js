cb9f27d0c7c51204d12e07e5d230b129ca804d53
VISHAL55UIET <vishalsingh962849@gmail.com>
2025-08-15 22:49:26 +0530

    base url

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/My-Portfolio-Website/', // <-- Add this line for GitHub Pages
+   base: '/Vishal-Portfolio-Website/', // <-- Add this line for GitHub Pages
   plugins: [react()],
 })
